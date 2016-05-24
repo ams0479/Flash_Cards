2a49ae5c3c72f034619776b1b9dd04c150869f6d
ams0479 <ams0479@gmail.com>
2016-05-23 21:31:57 -0500

    add initial html, css, and js files

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+  $("#JS").click(function() {
+    alert("Description of JS");
+  });
+
+  $("#Operators").click(function() {
+    alert("Description of Operators");
+  });
+
+  $("#Variables").click(function() {
+    alert("Description of Variables");
+  });
+
+  $("#Functions").click(function() {
+    alert("Description of Functions");
+  });
+
+  $("#Methods").click(function() {
+    alert("Description of Methods");
+  });
+
+  $("#Arguments").click(function() {
+    alert("Description of Arguments");
+  });
+
+  $("#Parameters").click(function() {
+    alert("Description of Parameters");
+  });
+
+  $("#Return").click(function() {
+    alert("Description of Return");
+  });
+
+  $("#Chaining_Methods").click(function() {
+    alert("Description of Chaining Methods");
+  });
+
+  $("#Strings").click(function() {
+    alert("Description of Strings");
+  });
+
+  $("#Booleans").click(function() {
+    alert("Description of Booleans");
+  });
+
+  $("#Undefined").click(function() {
+    alert("Description of Undefined");
+  });
+});
