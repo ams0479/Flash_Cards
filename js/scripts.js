35e15da74311471584a4d27a90747eb19f1f4980
ams0479 <ams0479@gmail.com>
2016-05-30 07:54:23 -0500

    more practice with jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,62 @@
 $(document).ready(function() {
- $(".side-one").click(function() {
-   $(".side-two").toggle();
-   $(".side-one").toggle();
+
+ $("#side-one-JS").click(function() {
+   $("#side-two-JS").toggle();
+   $("#side-one-JS").toggle();
+  });
+
+ $("#side-two-JS").click(function() {
+   $("#side-one-JS").toggle();
+   $("#side-two-JS").toggle();
    });
 
- $(".side-two").click(function() {
-   $(".side-one").toggle();
-   $(".side-two").toggle();
+ $("#side-one-Op").click(function() {
+   $("#side-two-Op").toggle();
+   $("#side-one-Op").toggle();
    });
+
+ $("#side-two-Op").click(function() {
+   $("#side-one-Op").toggle();
+   $("#side-two-Op").toggle();
+   });
+
+ $("#side-one-Var").click(function() {
+   $("#side-two-Var").toggle();
+   $("#side-one-Var").toggle();
+   });
+
+ $("#side-two-Var").click(function() {
+   $("#side-one-Var").toggle();
+   $("#side-two-Var").toggle();
+   });
+
+ $("#side-one-Func").click(function() {
+   $("#side-two-Func").toggle();
+   $("#side-one-Func").toggle();
+   });
+
+ $("#side-two-Func").click(function() {
+   $("#side-one-Func").toggle();
+   $("#side-two-Func").toggle();
+   });
+
+   $("#side-one-Meth").click(function() {
+     $("#side-two-Meth").toggle();
+     $("#side-one-Meth").toggle();
+     });
+
+   $("#side-two-Meth").click(function() {
+     $("#side-one-Meth").toggle();
+     $("#side-two-Meth").toggle();
+     });
+
+   $("#side-one-Arg").click(function() {
+     $("#side-two-Arg").toggle();
+     $("#side-one-Arg").toggle();
+     });
+
+   $("#side-two-Arg").click(function() {
+     $("#side-one-Arg").toggle();
+     $("#side-two-Arg").toggle();
+     });
 });
